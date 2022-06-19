d8e923b0c517326c29a492ab0f6c32843cb244dc
exoutia <jhav365@gmail.com>
2022-06-19 11:27:41 +0530

    Done

---
+++ python_audio_basics.py
@@ -0,0 +1,13 @@
+# Audio File Formats 
+# .mp3 // Compressed format
+# .flac // Can be converted into original 
+# .wav // original file with every property of audio
+
+import wave
+
+# Audio Signal Parameters
+# - Number of channels -> 1 is mono - 2 is stereo
+# - sample width
+# - framerate/ sample_rate: 44,000 Hz (standard cd sample)
+# - numbers of frames
+# - Values of a frame
