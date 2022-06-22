77a66ed91814b61068523264570a12037a1b3763
Exoutia <jhav365@gmail.com>
2022-06-22 18:17:54 +0530

    done

---
+++ speech_to_text/main.py
@@ -0,0 +1,15 @@
+import json
+import requests
+from api_key import API_KEY
+
+from api_comunicate import upload, transcription, get_transcription_result, save_file
+
+import sys
+
+filename = sys.argv[1]
+
+
+audio_url = upload(filename)
+job_id = transcription(audio_url)
+data = get_transcription_result(job_id)
+filename_save = save_file(data, filename);
