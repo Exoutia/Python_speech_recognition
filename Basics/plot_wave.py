d694a5e42deb901d9d2dbe967e0da793cba6e484
Exoutia <jhav365@gmail.com>
2022-06-22 15:42:59 +0530

    done

--- Basics/plot_wave.py
+++ Basics/plot_wave.py
@@ -2,7 +2,7 @@ import wave
 import matplotlib.pyplot as plt
 import numpy as np
 
-obj = wave.open("sample.wav", "rb")
+obj = wave.open("output.wav", "rb")
 
 sample_freq = obj.getframerate()
 n_sample = obj.getnframes()
@@ -15,14 +15,14 @@ print(t_audio)
 
 signal_arr = np.frombuffer(sample_wave, dtype=np.int16)
 
-# time = np.linspace(0, t_audio, num = n_sample)
+time = np.linspace(0, t_audio, num = n_sample)
 
-# plt.figure(figsize=(15,5))
-# plt.plot(time, signal_arr)
-# plt.title("Audio Signal")
-# plt.ylabel("Amplitude")
-# plt.xlabel("Time")
-# plt.xlim(0, t_audio)
-# plt.show()
+plt.figure(figsize=(15,5))
+plt.plot(time, signal_arr)
+plt.title("Audio Signal")
+plt.ylabel("Amplitude")
+plt.xlabel("Time")
+plt.xlim(0, t_audio)
+plt.show()
 
 
